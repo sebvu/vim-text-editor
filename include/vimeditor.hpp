34e2c07a2608c1c15d8f78603165d9c55146df5a
sebvu <santosjester738@gmail.com>
2025-09-10 11:27:53 -0500

    Create modal modes and cleanup code logic

--- include/vimeditor.hpp
+++ include/vimeditor.hpp
@@ -1,6 +1,8 @@
 #ifndef VIMEDITOR_HPP
 #define VIMEDITOR_HPP
+#include <functional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class VimEditor {
@@ -9,6 +11,32 @@ private:
   static constexpr int MIN_Y = 0;
   int curr_x = MIN_X, curr_y = MIN_Y, ch;
   std::vector<std::string> lines = {""};
+
+  struct EnumClassHash {
+    template <typename T> std::size_t operator()(T t) const {
+      return static_cast<std::size_t>(t);
+    }
+  };
+
+  enum class Mode { NORMAL, INSERT, COMMAND, VISUAL };
+
+  std::unordered_map<Mode, std::function<void(int)>, EnumClassHash> modeActions;
+
+  Mode mode;
+  static std::string modeToString(Mode m) {
+    switch (m) {
+    case Mode::NORMAL:
+      return "NORMAL";
+    case Mode::INSERT:
+      return "INSERT";
+    case Mode::COMMAND:
+      return "COMMAND";
+    case Mode::VISUAL:
+      return "VISUAL";
+    default:
+      return "UNKNOWN";
+    }
+  }
 
   void chHandler(int c);
   void renderLines();
--- src/vimeditor.cpp
+++ src/vimeditor.cpp
@@ -4,68 +4,96 @@
 #include <string>
 #include <vector>
 
-VimEditor::VimEditor() {}; // include file loading later
+VimEditor::VimEditor() {
+  modeActions[Mode::NORMAL] = [this](int c) {
+    switch (c) {
+    case 104: // h
+      if (curr_x > MIN_X)
+        curr_x -= 1;
+      break;
+    case 107: // k
+      if (curr_y > MIN_Y) {
+        curr_y -= 1;
+        curr_x =
+            std::min(static_cast<int>(lines[curr_y].size()) + MIN_X, curr_x);
+      }
+      break;
+    case 106: // j
+      if (curr_y < static_cast<int>(lines.size()) - 1) {
+        curr_y += 1;
+        curr_x =
+            std::min(static_cast<int>(lines[curr_y].size()) + MIN_X, curr_x);
+      }
+      break;
+    case 108: // l
+      if (curr_x < static_cast<int>(lines[curr_y].size()) + MIN_X)
+        curr_x += 1;
+      break;
+    case 105: // i INSERT MODE
+      mode = Mode::INSERT;
+      break;
+    default:
+      break;
+    }
+  };
+
+  modeActions[Mode::INSERT] = [this](int c) {
+    switch (c) {
+    case 27: // esc NORMAL MODE
+      mode = Mode::NORMAL;
+      break;
+    case KEY_BACKSPACE: // backspace
+      if (curr_x > MIN_X) {
+        lines[curr_y].erase(lines[curr_y].begin() + curr_x - MIN_X - 1);
+        curr_x -= 1;
+        // delch();
+      } else if (curr_y > MIN_Y) { // append curr line to prev line. no action
+                                   // if at 0th y line
+        curr_x = static_cast<int>(lines[curr_y - 1].size()) + MIN_X;
+        lines[curr_y - 1] += lines[curr_y];
+        lines.erase(lines.begin() + curr_y);
+        curr_y -= 1;
+      }
+      break;
+    case '\n':
+      lines.push_back("");
+      curr_y += 1;
+      curr_x = MIN_X;
+      break;
+
+    default:
+      lines[curr_y].insert(lines[curr_y].begin() + curr_x - MIN_X, ch);
+      curr_x += 1;
+      break;
+    };
+  };
+  // modeActions[Mode::COMMAND] = [this](int c) {}
+  // modeActions[Mode::VISUAL] = [this](int c) {}
+
+  curr_x = MIN_X;
+  curr_y = MIN_Y;
+  mode = Mode::NORMAL; // default mode
+
+}; // include file loading later
 
 void VimEditor::chHandler(int c) {
-  // movement rules
-  switch (c) {
-  case KEY_LEFT: // left arrow
-    if (curr_x > MIN_X)
-      curr_x -= 1;
-    break;
-  case KEY_UP: // up arrow
-    if (curr_y > MIN_Y) {
-      curr_y -= 1;
-      curr_x = std::min(static_cast<int>(lines[curr_y].size()) + MIN_X, curr_x);
-    }
-    break;
-  case KEY_DOWN: // down arrow
-    if (curr_y < static_cast<int>(lines.size()) - 1) {
-      curr_y += 1;
-      curr_x = std::min(static_cast<int>(lines[curr_y].size()) + MIN_X, curr_x);
-    }
-    break;
-  case KEY_RIGHT: // right arrow
-    if (curr_x < static_cast<int>(lines[curr_y].size()) + MIN_X)
-      curr_x += 1;
-    break;
-  case KEY_BACKSPACE: // backspace
-    if (curr_x > MIN_X) {
-      lines[curr_y].erase(lines[curr_y].begin() + curr_x - MIN_X - 1);
-      curr_x -= 1;
-      // delch();
-    } else if (curr_y > MIN_Y) { // append curr line to prev line. no action
-                                 // if at 0th y line
-      curr_x = static_cast<int>(lines[curr_y - 1].size()) + MIN_X;
-      lines[curr_y - 1] += lines[curr_y];
-      lines.erase(lines.begin() + curr_y);
-      curr_y -= 1;
-    }
-    break;
-  case '\n': // append new line with enter
-    lines.push_back("");
-    curr_y += 1;
-    curr_x = MIN_X;
-    break;
-  default:
-    // addch(ch);
-    lines[curr_y].insert(lines[curr_y].begin() + curr_x - MIN_X, ch);
-    curr_x += 1;
-    // getyx(stdscr, curr_y, curr_x); // update y, x to ncurses' real cursor
-    // pos
-    break;
+
+  auto it = modeActions.find(mode);
+  if (it != modeActions.end()) {
+    it->second(c); // execute mode-specific lambda
   }
 }
 
 void VimEditor::renderLines() {
-  for (int i = 0; i < static_cast<int>(lines.size()); i++) {
+  for (int i = 0; i < static_cast<int>(lines.size()) && i < LINES - 2; i++) {
     int rel = i - curr_y; // relative line pos
     if (i == curr_y) {
-      mvprintw(i, 0, "%2d %s", 0, lines[i].c_str());
+      mvprintw(i, 0, "%2d %s", i, lines[i].c_str());
     } else {
       mvprintw(i, 0, "%2d %s", abs(rel), lines[i].c_str());
     }
   }
+  mvprintw(LINES - 1, 0, "%s", modeToString(mode).c_str()); // printout mode
 }
 
 void VimEditor::runEditor() {
@@ -74,6 +102,8 @@ void VimEditor::runEditor() {
   initscr();
   keypad(stdscr, TRUE); // enable keypad mode on curr window
   noecho();             // doesn't echo clicking random keys
+
+  set_escdelay(25); // here as a patch solution for delayed esc press
 
   do {
     clear(); // clears current window (stdscr)
